4624914eddec2aae387c29aaee8d783a452a041a
nqdinh-1203 <quangdinhnt1203@gmail.com>
2023-03-30 21:49:07 +0700

    first commit

---
+++ scripts/deploy.ts
@@ -0,0 +1,29 @@
+import { ethers, hardhatArguments } from "hardhat";
+import * as Config from "./config";
+
+import * as dotenv from 'dotenv' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+dotenv.config()
+
+async function main() {
+  await Config.initConfig();
+  const network = hardhatArguments.network ? hardhatArguments.network : "dev";
+  const [deployer] = await ethers.getSigners();
+  console.log("Deploying from address: ", deployer.address);
+
+  const Demo = await ethers.getContractFactory("Demo");
+  const demo = await Demo.deploy();
+  await demo.deployed();
+
+  console.log("Demo Contract address: ", demo.address);
+  Config.setConfig(network + '.Demo', demo.address);
+
+  await Config.updateConfig();
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
